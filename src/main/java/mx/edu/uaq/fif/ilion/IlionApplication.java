1fe4afd99fbe574dc9ca6276e426aafcc70adb3e
cvaldez27 <cvaldez27@alumnos.uaq.mx>
2026-05-04 18:22:41 -0600

    feat: initialize Ilion with Spring Boot, Security and PostgreSQL dev/prod profiles

---
+++ src/main/java/mx/edu/uaq/fif/ilion/IlionApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.uaq.fif.ilion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/uaq/fif/ilion/IlionApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.uaq.fif.ilion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
